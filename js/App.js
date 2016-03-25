90cab3d31d559b9e0d700f72f0aead9349c726f8
webFarrock <webFarrock@mail.ru>
2016-03-25 08:10:02 +0300

    сортировки и частично поиск по имени

--- js/App.js
+++ js/App.js
@@ -9,23 +9,51 @@ export default class App extends Component {
         super(props);
         this.state = {
             usersData: [],
-            curUserId: false
+            curUserId: false,
+            sortBy: false,
+            order: false,
+            searchByNameValue: '',
         };
 
         this.loadUsersData();
     }
 
+    sortByName(){
+        const order = !this.state.order;
+        const usersData = this.state.usersData.sort((a, b) => {return (order ? 1 : -1)*((a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0)});
+        const curUserId = usersData[0].id;
+
+        this.setState({
+            order: order,
+            usersData: usersData,
+            curUserId: curUserId
+        });
+    }
+
+    sortByAge(){
+        const order = !this.state.order;
+        const usersData = this.state.usersData.sort((a, b) => (order ? 1 : -1)*(a.age - b.age));
+        const curUserId = usersData[0].id;
+
+        this.setState({
+            order: order,
+            usersData: usersData,
+            curUserId: curUserId
+        });
+    }
+
     loadUsersData(){
+
         axios.get('../data.json')
              .then(data => {
                  let curUserId = false;
-                 
+
                  if (data.data instanceof Array && data.data.length){
                      curUserId = data.data[0].id;
                  }
-                 console.log('curUserId = '+curUserId);
-                 
+
                  this.setState({
+                    usersDataRaw: data.data,
                     usersData: data.data,
                     curUserId: curUserId,
                 });
@@ -41,12 +69,25 @@ export default class App extends Component {
         });
     }
 
+    searchByName(val){
+        console.log(val);
+        
+        let usersData = this.state.usersDataRaw.filter((item) => {if(-1 !== item.name.indexOf(val)) return false; });
+
+        const curUserId = usersData[0] ? usersData[0].id : false;
+        this.setState({
+            searchByNameValue: val,
+            usersData: usersData,
+            curUserId: curUserId
+        });
+    }
+
 
     render() {
         return (
             <div className="container-fluid app">
-                <SearchBar />
-                <ToolBar />
+                <SearchBar searchByNameValue={this.state.searchByNameValue} searchByName={this.searchByName.bind(this)} />
+                <ToolBar sortByName={this.sortByName.bind(this)} sortByAge={this.sortByAge.bind(this)} />
                 <User selectUser={this.selectUser.bind(this)} usersData={this.state.usersData} curUserId={this.state.curUserId} />
             </div>
         );
--- js/components/SearchBar.js
+++ js/components/SearchBar.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
-export default () => {
-  return (
-    <div className="row">
-        <div className="col-sm-12">
-            <div className="searchbar">
-                <input value="" type="text" className="form-control" placeholder="Search people by name..." />
+export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+    }
+    
+    searchByName(e){
+        this.props.searchByName(e.target.value);
+    }
+    
+    render(){
+          return (
+            <div className="row">
+                <div className="col-sm-12">
+                    <div className="searchbar">
+                        <input value={this.props.searchByNameValue} onChange={this.searchByName.bind(this)} type="text" className="form-control" placeholder="Search people by name..." />
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  );
+          );
+    }
 }
 
 
